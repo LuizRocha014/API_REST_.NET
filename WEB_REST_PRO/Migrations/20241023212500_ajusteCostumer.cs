29c8225638e003b96124356bfabdaef06e7a9939
LuizRocha <luizaugusto.clima@gmail.com>
2024-10-23 21:31:19 -0300

    feat: migration tabela Costumers

---
+++ WEB_REST_PRO/Migrations/20241023212500_ajusteCostumer.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WEB_REST_PRO.Migrations
+{
+    /// <inheritdoc />
+    public partial class ajusteCostumer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Cpf",
+                table: "Customer",
+                newName: "Reference");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "Customer",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AddressNumber",
+                table: "Customer",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Cnpj",
+                table: "Customer",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Customer",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "Customer");
+
+            migrationBuilder.DropColumn(
+                name: "AddressNumber",
+                table: "Customer");
+
+            migrationBuilder.DropColumn(
+                name: "Cnpj",
+                table: "Customer");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Customer");
+
+            migrationBuilder.RenameColumn(
+                name: "Reference",
+                table: "Customer",
+                newName: "Cpf");
+        }
+    }
+}
