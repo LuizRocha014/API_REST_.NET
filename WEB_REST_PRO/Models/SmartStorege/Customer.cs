004a89b4818ac55039ad4c266540e4046b49a3ce
LuizRocha <luizaugusto.clima@gmail.com>
2024-10-23 21:31:52 -0300

    fix: ajuse na tabela Costumers

--- WEB_REST_PRO/Models/SmartStorege/Customer.cs
+++ WEB_REST_PRO/Models/SmartStorege/Customer.cs
@@ -7,9 +7,17 @@ public class Customer : Core
 
     public string Name { get; set; }
 
-    public string Cpf { get; set; }
+    public string Cnpj { get; set; }
 
     public string Phone { get; set; }
 
-    public string ZipCode{ get; set;}
+    public string ZipCode { get; set; }
+
+    public string Address { get; set; }
+
+    public string AddressNumber { get; set; }
+
+    public string Email { get; set; }
+    public string Reference { get; set; }
 }
+
