b6add47a79f0d4e46459f8379562cc0dc755763c
LuizRocha <luizaugusto.clima@gmail.com>
2024-11-05 23:44:18 -0300

    feat: novos controllers

---
+++ WEB_REST_PRO/Controllers/SmartStorege/ProductController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WEB_REST_PRO.Data.Interface.SmartStorege;
+using WEB_REST_PRO.Data.Repository.SmartStorege;
+
+namespace WEB_REST_PRO.Controllers.SmartStorege
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController
+    {
+        private IProductRepository _productRepository;
+        public ProductController(IProductRepository productRepository)
+        {
+                _productRepository = productRepository;
+        }
+
+        [Route("GetAll")]
+        [HttpGet]
+        public IActionResult getAllShopProduct( DateTime? ultDate)
+        {
+            try
+            {
+                var retorno = _productRepository.GetAll(ultDate);
+                return new JsonResult(retorno);
+            }
+            catch (Exception)
+            {
+
+                return new JsonResult(false);
+            }
+        }
+
+        [Route("PostAll")]
+        [HttpPost]
+        public IActionResult PostAll([FromBody] List<Product> listCustomer)
+        {
+            try
+            {
+
+
+                return new JsonResult(_productRepository.AddShopProduct(listCustomer));
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
---
+++ WEB_REST_PRO/Controllers/SmartStorege/RolePermissionController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WEB_REST_PRO.Data.Interface.SmartStorege;
+
+namespace WEB_REST_PRO.Controllers.SmartStorege
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RolePermission
+    {
+        private IRolePermissionRepository _rolePermissionRepository;
+
+        public RolePermission(IRolePermissionRepository rolePermissionRepositor)
+        {
+            _rolePermissionRepository = rolePermissionRepositor;
+        }
+
+        //[Route("AddNewShop")]
+        //[HttpPost]
+        //public IActionResult PostAll([FromBody] List<RolePermission> listCustomer)
+        //{
+        //    try
+        //    {
+
+
+        //        return new JsonResult(_rolePermissionRepository.AddShopProduct(listCustomer));
+        //    }
+        //    catch (Exception)
+        //    {
+
+        //        throw;
+        //    }
+        //}
+        [Route("GetAll")]
+        [HttpGet]
+        public IActionResult GetAllCategory( DateTime? ultDate)
+        {
+            try
+            {
+                return new JsonResult(_rolePermissionRepository.GetAllPermission(ultDate));
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
