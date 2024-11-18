8d5db4b1172593a7c26c3b51ad8b02489ccaf62c
LuizRocha <luizaugusto.clima@gmail.com>
2024-11-17 22:30:16 -0300

    feat: migrations

---
+++ WEB_REST_PRO/Controllers/SmartStorege/TransactionController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WEB_REST_PRO.Data.Interface.SmartStorege;
+using WEB_REST_PRO.Data.Repository.SmartStorege;
+using WEB_REST_PRO.Models.SmartStorege;
+
+namespace WEB_REST_PRO.Controllers.SmartStorege
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TransactionController
+    {
+        private ITransactionRepository _repository;
+        public TransactionController(ITransactionRepository repsoitory)
+        {
+            _repository = repsoitory;
+        }
+
+        [Route("PostAll")]
+        [HttpPost]
+        public IActionResult PostAll([FromBody] List<Transactions> listCustomer)
+        {
+            try
+            {
+
+
+                return new JsonResult(_repository.AddShopProduct(listCustomer));
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        [Route("GetAll")]
+        [HttpGet]
+        public IActionResult GetAllCategory(DateTime? ultDate)
+        {
+            try
+            {
+                return new JsonResult(_repository.GetAllPermission(ultDate));
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
---
+++ WEB_REST_PRO/Migrations/20241117225222_ajusteShopId.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WEB_REST_PRO.Migrations
+{
+    /// <inheritdoc />
+    public partial class ajusteShopId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Storedlocation",
+                table: "Product",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ShopId",
+                table: "Product",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ShopId",
+                table: "Customer",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ShopId",
+                table: "Product");
+
+            migrationBuilder.DropColumn(
+                name: "ShopId",
+                table: "Customer");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Storedlocation",
+                table: "Product",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+        }
+    }
+}
