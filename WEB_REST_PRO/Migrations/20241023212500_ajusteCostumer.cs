3f228814e4b2d3c762cbb7f0602453a1de6bca37
ROCHA\luiza <luizaugusto.clima@gmail.com>
2025-07-27 21:25:44 -0300

    feat: adicionado dockerFlile

--- WEB_REST_PRO/Migrations/20241023212500_ajusteCostumer.cs
+++ WEB_REST_PRO/Migrations/20241023212500_ajusteCostumer.cs
@@ -7,7 +7,7 @@ namespace WEB_REST_PRO.Migrations
     /// <inheritdoc />
     public partial class ajusteCostumer : Migration
     {
-        /// <inheritdoc />
+        /// <inheritdoc />  
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.RenameColumn(
